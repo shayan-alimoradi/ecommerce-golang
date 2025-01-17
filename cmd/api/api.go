a9476004cc4711b12eacf11db9f9ac2364f1c2da
unknown <shayan.aimoradii@gmail.com>
2025-01-17 16:33:35 +0330

    feat: Implement user service

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -2,9 +2,11 @@ package api
 
 import (
 	"database/sql"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/shayan-alimoradi/ecommerce-golang/service/user"
 )
 
 type APIServer struct {
@@ -18,6 +20,12 @@ func NewAPIServer(addr string, db *sql.DB) *APIServer {
 
 func (s *APIServer) Run() error {
 	router := mux.NewRouter()
+	subrouter := router.PathPrefix("/api/v1").Subrouter()
+
+	userHandler := user.NewHandler()
+	userHandler.RegisterRoutes(subrouter)
+
+	log.Println("Listening on", s.addr)
 
 	return http.ListenAndServe(s.addr, router)
 }
--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"log"
+
 	"github.com/shayan-alimoradi/ecommerce-golang/cmd/api"
 )
 
 func main() {
 	server := api.NewAPIServer(":8081", nil)
 	if err := server.Run(); err != nil {
-		return
+		log.Fatal(err)
 	}
 }
